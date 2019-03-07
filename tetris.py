c746e2e5e0dd2faf45fb038b13a8f9b4f76dddc2
TaylorL19 <tlockhart@tamu.edu>
2019-03-06 18:36:24 -0600

    initial commit of tetris with falling blocks

---
+++ tetris.py
@@ -0,0 +1,35 @@
+import cv2
+import pygame
+import time
+
+from tile import Tile
+
+TILE_LENGTH = Tile.sidelength
+WIDTH = 300
+HEIGHT = 500
+BOARD_WIDTH = WIDTH/TILE_LENGTH
+BOARD_HEIGHT = HEIGHT/TILE_LENGTH
+BG = (255,255,255)
+
+screen = pygame.display.set_mode((WIDTH, HEIGHT))
+screen.fill(BG)
+
+tiles = []
+
+tiles.append(Tile(1,1, (255,0,0)))
+tiles.append(Tile(2,2, (0,255,0)))
+def tick():
+    screen.fill(BG)
+    for tile in tiles:
+        tile.update()
+        tile.draw(screen)
+    pygame.display.flip()
+
+running = True
+while running:
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT:
+            running = False
+    #30 frames per second
+    time.sleep(0.032)
+    tick()
---
+++ tile.py
@@ -0,0 +1,20 @@
+import pygame
+import time
+
+WIDTH = 300
+HEIGHT = 500
+
+class Tile:
+    sidelength = 10
+    def __init__(self, x = 0, y = 0, color = (0,0,0)):
+        self.x = x
+        self.y = y
+        self.color = color
+    
+    def draw(self, screen):
+        pygame.draw.rect(screen, self.color, pygame.Rect(self.x * Tile.sidelength, self.y * Tile.sidelength, 10, 10))
+
+    def update(self):
+        print(self.x, self.y, self.x * Tile.sidelength, self.y * Tile.sidelength)
+        if self.y * Tile.sidelength + Tile.sidelength < HEIGHT:
+            self.y += 1
